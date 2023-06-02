ac50ffdaf4782a7eba0f5ee739a589508f41ccf3
James Chan <jameschan312.cn@gmail.com>
2023-06-02 16:23:59 -0400

    feat: built a SpringBoot3 scaffold

--- src/main/java/us/jameschan/supervisor/advice/SupervisorInterceptor.java
+++ src/main/java/us/jameschan/supervisor/advice/SupervisorInterceptor.java
@@ -40,7 +40,7 @@ public class SupervisorInterceptor implements HandlerInterceptor {
         // Enable cors.
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
 
